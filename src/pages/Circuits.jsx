ed7caab92bc820ab33967961620cf93c2eb09123
roshith <roshithprakash7@gmail.com>
2024-11-03 15:23:46 +0530

    Loading placeholder for circuits modified

--- src/pages/Circuits.jsx
+++ src/pages/Circuits.jsx
@@ -148,14 +148,14 @@ const LoadingTableCard = () => {
                     <TableCell className="font-medium py-3 px-3 md:w-[5em]">
                       <div className="bg-gray-300 animate-pulse w-10 h-5 rounded"></div>
                     </TableCell>
-                    <TableCell className="px-2">
-                      <div className="bg-gray-300 animate-pulse w-[90%] h-5 rounded"></div>
+                    <TableCell className="px-2 w-36">
+                      <div className="bg-gray-300 animate-pulse h-5 rounded"></div>
                     </TableCell>
-                    <TableCell className="px-2">
-                      <div className="bg-gray-300 animate-pulse w-[40%] h-5 rounded"></div>
+                    <TableCell className="px-2 w-36">
+                      <div className="bg-gray-300 animate-pulse h-5 rounded"></div>
                     </TableCell>
-                    <TableCell className="px-2">
-                      <div className="bg-gray-300 animate-pulse w-[40%] h-5 rounded"></div>
+                    <TableCell className="px-2 w-36">
+                      <div className="bg-gray-300 animate-pulse h-5 rounded"></div>
                     </TableCell>
                     <TableCell className="gap-x-2 px-2 py-2 text-nowrap">
                       <div className="bg-gray-300 animate-pulse w-full min-w-20 h-52 rounded"></div>
